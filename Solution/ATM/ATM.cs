70e7fe163c8faca88880a64a4f56d878a84249af
Stacey Teltser <stacey.teltser@gmail.com>
2020-05-06 01:25:34 -0500

    withdraw money failed and now passing

--- Solution/ATM/ATM.cs
+++ Solution/ATM/ATM.cs
@@ -5,20 +5,35 @@ namespace ATM
     {
         private int balance;
 
-        // Constructor
+        // Default constructor for ATM
         public ATM()
         {
             // Initialize the inital balance as 0
             balance = 0;
         }
 
+        // ATM constructor with default balance
+        public ATM(int defaultBalance)
+        {
+            balance = defaultBalance;
+        }
+
+        // View Balance method
         public int ViewBalance()
         {
             return balance;
         }
+
+        // Add Money method
         public void DepositMoney(int depositAmount)
         {
             balance += depositAmount;
+        }
+
+        // Withdraw Money method
+        public void WithdrawMoney(int withdrawAmount)
+        {
+            balance -= withdrawAmount;
         }
     }
 }
--- Solution/ATMTests/ATMTests.cs
+++ Solution/ATMTests/ATMTests.cs
@@ -13,6 +13,7 @@ namespace ATM
             Assert.Equal(0, atm.ViewBalance());
         }
 
+        // Test adding money including subsequent deposits
         [Theory]
         [InlineData(5, 6, 11)]
         [InlineData(100, 101, 201)]
@@ -23,6 +24,19 @@ namespace ATM
             // Calling DepositMoney with $5
             atm.DepositMoney(depositAmount1);
             atm.DepositMoney(depositAmount2);
+            Assert.Equal(expected, atm.ViewBalance());
+        }
+
+        // Test for withdrawing money
+        [Theory]
+        [InlineData(7, 4, 3)]
+        [InlineData(100, 10, 90)]
+        public void WithdrawMoneyTest(int initialBalance, int withdrawAmount, int expected)
+        {
+            // Create an instance of ATM class by calling ATM constructor
+            ATM atm = new ATM(initialBalance);
+            // Calling WithdrawMoney with amounts passed in
+            atm.WithdrawMoney(withdrawAmount);
             Assert.Equal(expected, atm.ViewBalance());
         }
     }
