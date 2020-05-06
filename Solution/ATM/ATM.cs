3f48ea3aa9d6ab984834c35c409c6b2bb89bf9c6
Stacey Teltser <stacey.teltser@gmail.com>
2020-05-06 00:44:52 -0500

    viewbalance is written to pass isZero test

--- Solution/ATM/ATM.cs
+++ Solution/ATM/ATM.cs
@@ -6,12 +6,16 @@ namespace ATM
         // Constructor
         public ATM()
         {
-
+            // Initialize the inital balance as 0
+            Balance = 0;
         }
+
+        // Creating a Balance property/variable that can change in value and be retrieved
+        private int Balance { get; set; }
 
         public int ViewBalance()
         {
-            return 5;
+            return Balance;
         }
     }
 }
