f73b17dc33d20775907074eae21711a46a7becd7
Stacey Teltser <stacey.teltser@gmail.com>
2020-05-05 22:34:02 -0500

    created project file structure

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab02_Unit_Tests_and_Documentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
